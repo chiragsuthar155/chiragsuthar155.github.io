4ef095c174dbb80143f4a854c67b929c524de036
chiragsuthar155 <94690296+chiragsuthar155@users.noreply.github.com>
2022-06-15 16:53:41 +0530

    Update script.js

--- Rock_Paper_Scissor/script.js
+++ Rock_Paper_Scissor/script.js
@@ -30,9 +30,9 @@ const cpuMove = function () {
 
 // Checking winning Condition; // Getting Problem with the logic
 function win(ele1, ele2) {
-  const a = Number(ele1.src[58]);
+  const a = Number(ele1.src[50]);
 
-  const b = Number(ele2.src[58]);
+  const b = Number(ele2.src[50]);
 
   if (a === 0 && b === 1) {
     score[1] += 1;
