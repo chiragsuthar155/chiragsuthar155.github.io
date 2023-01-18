89efd228e625f3fcbc3a487e21c00ea72e3e829e
chiragsuthar155 <94690296+chiragsuthar155@users.noreply.github.com>
2023-01-19 00:58:16 +0530

    todo

---
+++ Todo_List/script.js
@@ -0,0 +1,39 @@
+const inputField = document.querySelector(".input");
+const btn = document.querySelector(".btn");
+const results = document.querySelector(".results");
+
+btn.addEventListener("click", function () {
+  let value = inputField.value;
+  results.insertAdjacentHTML(
+    "afterbegin",
+    `
+        <div class="entry">
+        <input class="checkbox" type="checkbox" />
+        <h2 class="entry-title">${value.toUpperCase()}</h3>
+        <button type="button" class="delete">Delete</button>
+        </div>
+    `
+  );
+  inputField.value = "";
+
+  const deleteBtn = document.querySelector(".delete");
+  const checkbox = document.querySelector(".checkbox");
+
+  console.log(deleteBtn, checkbox, results);
+
+  deleteBtn.addEventListener("click", function (e) {
+    const deleteEntry = e.target.parentNode;
+    deleteEntry.remove();
+  });
+
+  checkbox.addEventListener("click", function (e) {
+    const checkItem = e.target;
+    const entryDone = e.target.nextElementSibling;
+
+    if (checkItem.checked == true) {
+      entryDone.style.textDecoration = "line-through";
+    } else {
+      entryDone.style.textDecoration = "none";
+    }
+  });
+});
